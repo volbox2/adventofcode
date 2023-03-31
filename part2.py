a272e9922c2c758f69a2ff0ca38d7fc8b934539f
volbox1902 <ngson1902@gmail.com>
2023-03-31 09:27:50 +0200

    Commented part2

--- part2.py
+++ part2.py
@@ -1,4 +1,4 @@
-def crt(input_file: str):
+def crt(input_file: str,width=40):
     x = 1
     row_pos = 0
     crt = list()
@@ -10,8 +10,10 @@ def crt(input_file: str):
                 line.append("#")
             else:
                 line.append(".")
+            # Position in the row starts at 0 and is incremented at the end of the cycle
             row_pos += 1
-            if row_pos%40 == 0:
+            # Check if a new line and a reset of the position are needed
+            if row_pos % width == 0:
                 crt.append(line)
                 line = []
                 row_pos = 0
@@ -22,7 +24,7 @@ def crt(input_file: str):
                     line.append(".")
                 row_pos +=1
                 x += int(inst.split()[1])
-                if row_pos%40 == 0:
+                if row_pos % width == 0:
                     crt.append(line)
                     line = []
                     row_pos = 0
