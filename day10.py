f77fea984488c0a8b7c2644df3ee705e37de30dd
volbox1902 <ngson1902@gmail.com>
2023-04-01 17:42:53 +0200

    Allowed user to give his own input file

--- day10.py
+++ day10.py
@@ -1,5 +1,13 @@
 from part1 import signal_strength
 from part2 import crt
+import optparse
+
+
+
+
 if __name__ == "__main__":
-    signal_strength("input.txt",20,40)
-    crt("input.txt")
+    parser = optparse.OptionParser()
+    parser.add_option("-i", action="store", type="string", dest="input", default="input.txt")
+    options, args = parser.parse_args()
+    signal_strength(options.input,20,40)
+    crt(options.input)
